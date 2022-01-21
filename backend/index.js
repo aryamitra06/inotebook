dff0c5afdba6b126cdb5b05fd142c8249b3a2568
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-01-22 02:13:16 +0530

    expressjs added to the index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 const connectToMongo = require('./db');
+const express = require('express')
 
 connectToMongo();
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+  console.log('Localhost running at port 3000')
+})
