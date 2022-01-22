aad86941eb55b185f8d8ceade0bd3a81250f529f
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-01-23 00:13:57 +0530

    created basic routes helping with epxress

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,9 @@ const express = require('express')
 connectToMongo();
 const app = express();
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(3000, () => {
   console.log('Localhost running at port 3000')
