1103251106852fe5ae2f6369deda8c4097ca1515
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-01-23 01:25:54 +0530

    making auth.js preventing duplicate email sign up.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const User = require('../models/User');
 // Create a user using POST method ('api/auth/')
 router.post('/', (req, res) => {
-    
+
     // Creating a new user instance
     const user = new User(req.body);
-    user.save();
 
+    try {
+        user.save();
+    } 
+    catch{
+        console.log("Username already exist!");
+    }
     // req.body takes the values as json from body
-    console.log(req.body);
+    // console.log(req.body);
 })
 
 module.exports = router;
