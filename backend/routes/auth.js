fe1b41866eeb1f7d9bd2cd47934c68fe27c06d67
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-01-23 09:35:33 +0530

    password hashing added to the register user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 
 // Create a User using: POST "/api/auth/createuser". No login required
@@ -23,10 +24,13 @@ async (req, res) => {
     if (user) {
       return res.status(400).json({ error: "Sorry a user with this email already exists" })
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedpass = await bcrypt.hash(req.body.password, salt);
     // Create a new user
     user = await User.create({
       name: req.body.name,
-      password: req.body.password,
+      password: hashedpass,
       email: req.body.email,
     })
     res.json(user)
