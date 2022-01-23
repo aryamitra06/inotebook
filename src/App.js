af48332125e892b104c10dab9493418e9de1f0e3
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-01-24 01:45:32 +0530

    page routes added (v1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <>
+      <Router>
+        <Switch>
+        <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React from "react";
+import {Link } from "react-router-dom";
 
 function Navbar() {
-  return( 
-  <>
-  This is navbar
-  </>
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <Link className="navbar-brand" to="/">
+                        iNotebook
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <li class="nav-item">
+                                <Link className="nav-link" to="/about">About</Link>
+                            </li>
+                            <div>
+                                <input type="text" className="form-control" placeholder="Search..." />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+        </>
     );
 }
 
