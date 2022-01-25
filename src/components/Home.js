64254987a3ed8b2e3d8d91e85d66e0cbd335c0fb
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-01-25 16:34:29 +0530

    making simple note fetching with hard code and context api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,34 @@
 import React from 'react';
-
+import { useContext } from 'react';
+import NoteContext from '../context/notes/NoteContext';
 function Home() {
+  const context = new useContext(NoteContext);
+  const {notes, setNotes} = context;
+
   return(
   <>
+  <div className="container my-3">
+  <h3>Add Note</h3>
+  <form action="" method="post">
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">Note Title</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">Note Description</label>
+          <textarea className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
+        </div>
+  <button className="btn btn-primary" type="submit">Add Note</button>      
+  </form>
+  </div>
+
+  <div className="container my-3">
+    <h3>My Notes</h3>
+    {notes.map((note) => {
+      return note.title;
+    })}
+  </div>
+  
   </>
   );
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import NoteContext from "./NoteContext";
-
 // defining the state
 const NoteState = (props) =>{
-    const state = {
-        "name": "Aryamitra",
-        "class": "10"
-    }
+    const notesInitial = [
+        {
+          "_id": "61ed5675ca1416de2cd7edf7",
+          "user": "61ed033baa9bff88608985c5",
+          "title": "this is the first note",
+          "description": "this app is nice!",
+          "tag": "anything",
+          "date": "2022-01-23T13:21:57.895Z",
+          "__v": 0
+        }
+      ]
+
+    const [notes, setNotes] = useState(notesInitial)
+
     return (
-        <NoteContext.Provider value = {state}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     );
